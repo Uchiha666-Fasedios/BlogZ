5e68c7fa57dbfa978546f55da67222b591521d56
adrian <adrianlisciotti@yahoo.com.ar>
2020-11-05 21:11:42 -0300

    buscador predictivo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -271,9 +271,14 @@
   <script src="{{ asset('assets/theme/js/script.js') }}"></script>
   <script src="{{ asset('assets/wowslider-effect/effects.js') }}"></script>
   <script src="{{ asset('assets/wowslider-init/script.js') }}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.bundle.min.js"></script>{{--libreria para q se desplege en elbuscador dicha caja de busqeda--}}
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/1.2.1/typeahead.bundle.min.js"></script>libreria para q se desplege en elbuscador dicha caja de busqeda--}}
+  {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/bloodhound.min.js"></script>--}}{{--libreria para q se desplege en elbuscador dicha caja de busqeda--}}
 <script src="{{ asset('js/buscador-predictivo.js') }}"></script>
 <script src="{{ asset('js/preloader.js') }}"></script>{{--para q ande la imagen de la carga--}}
+
+
+
+
 
 
 
