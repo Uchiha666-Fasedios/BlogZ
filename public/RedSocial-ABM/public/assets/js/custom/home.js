eb87da569d64060b4240a1e9ad30ea85d367eb18
adrian <adrianlisciotti@yahoo.com.ar>
2020-11-16 20:49:13 -0300

    red sosial symfony4 stylos mejorados2

--- public/RedSocial-ABM/public/assets/js/custom/home.js
+++ public/RedSocial-ABM/public/assets/js/custom/home.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     }));
 
     ias.extension(new IASSpinnerExtension({
-        src: URL + '../assets/images/ajax-loader.gif'
+        src: URL + '/RedSocial-ABM/../assets/images/ajax-loader.gif'
 
     }));
 
