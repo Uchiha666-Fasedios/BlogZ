bb148b9c340ec33a7f1688892de403cdf3b35617
adrian <adrianlisciotti@yahoo.com.ar>
2021-04-24 13:02:29 -0300

    eliminacion de articulos con tostada

--- resources/views/admin/articulos/index.blade.php
+++ resources/views/admin/articulos/index.blade.php
@@ -61,10 +61,11 @@
 				  	</a>
 				  </td>
 				  <td>
-				  	<form method="POST" action="{{ route('articulos.destroy',$articulo->id) }}">
+                    <form method="POST" action="{{ route('articulos.destroy',$articulo->id) }}">
                         @csrf
                         {{ method_field('DELETE') }}
-                        <button style=" background-color:white ;border:0" type="submit" onclick="return confirm('¿Estás Seguro?')">
+                        {{-- class="eliminar" pongo el indicador como clase y no como id porqe al ser un bucle se repetiria siempre ese id por cada articuklo y el id tiene q ser unico --}}
+                        <button class="eliminar" style=" background-color:white ;border:0" type="submit"> {{--onclick="return confirm('¿Estás Seguro?')"--}}
 				  			<img width="25px" src="{{ asset('imagenes/admin/eliminar.png') }}" alt="title 1" title="title 1">
 				  		</button>
 				  	</form>
@@ -80,4 +81,49 @@
 	</div>
 </div>
 
+@endsection
+
+
+{{--ELIMINACION CON AXIOS (axios es como ajax)--}}
+@section('articulos-css')
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+{{-- lo saqe de https://datatables.net/examples/styling/bootstrap3.html --}}
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+@endsection
+
+@section('articulos-js')
+<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+{{-- lo saqe de https://datatables.net/examples/styling/bootstrap3.html --}}
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+{{------------------------------------------------------------------------}}
+
+
+<script>
+    $(document).ready(function() {
+
+      $('.eliminar').click(function(e){//hago clik en el boton la e vendria ser el submin del formulario
+	e.preventDefault();//para q no recarge la pagina , evita q el submin recarge la pagina
+        if(!confirm("¿Estás seguro?")){//sale la ventanita de confirm y si es diferente a true (toqe cancelar) me retorna un false si pongo aceptar sigue el codigo
+            return false;
+        }
+		var form=$(this).parents('form');//this hace referencia al elemento .eliminar o sea el boton ..parents('form') cogeme el padre q el padre inmediato del boton es form
+        //var url='./admin/eliminar-todos-articulos';
+		var url=form.attr('action');//form. entonces del form .. attr('action')cogemos la url atraves del action
+
+        axios.delete(url).then(response =>{ //eliminamos
+        //las tostadas para el mensajito de eliminacion fue sacado de https://github.com/CodeSeven/toastr
+            toastr.success('El articulo ha sido eliminado','¡Bien!', {
+                "progressBar": true,
+                "positionClass": "toast-bottom-right",
+            });//fin de tostada*/
+           console.log('todo bien!!');
+        }).catch(error => {//si pesca un error
+            toastr.error('Error');
+        });
+            var row= $(this).parents('tr');//this del elemento boton.. parents('tr') cogemos el padre para luego eliminarlo (acordate q cada tr es un articulo porqe esta dentro de un bucle)
+        row.fadeOut();//fadeOut() metodo de  jquery ..con esto hacemos q desaparezca de una forma elegante
+    });
+    });
+
+    </script>
 @endsection
